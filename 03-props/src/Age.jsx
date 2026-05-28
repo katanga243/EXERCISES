1f7a7b59582b64029a65d3adb1e543dc5c44ea5b
Katanga Jean Dedieu <165053759+katanga243@users.noreply.github.com>
2026-05-28 23:03:24 +0200

    Add Age component to display age prop

---
+++ 03-props/src/Age.jsx
@@ -0,0 +1,11 @@
+// The Age component receives an age prop and renders the
+// "Your age is " message followed by the age value, inside a p tag.
+function Age({ age }) {
+  return (
+    <p>
+      Your age is {age}
+    </p>
+  );
+}
+
+export default Age;
